0b33089f04e0d483e76e5d80055a5bfce342f307
KorwinBieniek <korwin-bieniek@wp.pl>
2020-10-15 22:27:57 +0200

    JavaStart Java exercises -> basic data types

---
+++ src/pl/javastart/JavaStart_Zadania/TypyDanych_Liczby/DataTypes.java
@@ -0,0 +1,10 @@
+package pl.javastart.JavaStart_Zadania.TypyDanych_Liczby;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        double x = 10;
+        double y = 4;
+
+        System.out.println(x/y);
+    }
+}
