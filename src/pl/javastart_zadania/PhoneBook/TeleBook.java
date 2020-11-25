2e4870f7626afe0faba785fc53ee672169205263
KorwinBieniek <korwin-bieniek@wp.pl>
2020-11-25 07:54:36 +0100

    PhoneBook mini project. Creating a Contact class and preparing TeleBook class for future methods

---
+++ src/pl/javastart_zadania/PhoneBook/TeleBook.java
@@ -0,0 +1,14 @@
+package pl.javastart_zadania.PhoneBook;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TeleBook {
+    Map<String, Contact> contacts = new HashMap<>();
+
+    public void add(){}
+
+    public void find(){}
+
+    public void remove(){}
+}
