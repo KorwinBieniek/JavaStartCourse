37fca76a176bbeceb61772b556dac169ad851dbd
KorwinBieniek <korwin-bieniek@wp.pl>
2020-12-05 22:02:17 +0100

    Implementing add(String name, String number) method to TeleBook class. It allows the user to add a new contact to the map, it also checks if the data is entered properly.

--- src/pl/javastart_zadania/PhoneBook/TeleBook.java
+++ src/pl/javastart_zadania/PhoneBook/TeleBook.java
@@ -6,7 +6,23 @@ import java.util.Map;
 public class TeleBook {
     Map<String, Contact> contacts = new HashMap<>();
 
-    public void add(){}
+    public boolean add(String name, String number) {
+        if(name == null || number == null) {
+            throw new IllegalArgumentException("Name and number cannot be null");
+        }
+        if (name.isEmpty() || number.isEmpty()) {
+            throw new IllegalArgumentException("Name and number cannot be empty");
+        }
+        if (number.length() != 9) {
+            throw new IllegalArgumentException("Number needs to have 9 digits");
+        }
+        if(!contacts.containsKey(name)) {
+            contacts.put(name, new Contact(name, number));
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     public void find(){}
 
